0b32ce865b027dee6fc346a4248e8707b9364168
Alessio Serra <48280936+alessioserra@users.noreply.github.com>
2023-10-24 10:31:22 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/spark/bin/spark-submit --packages org.apache.spark:spark-avro_2.13:3.4.1 /home/proveAle/content/prova24.py 17 240297 "ProvaAlgoritmo" "{'hPacketId': 117,
+ 'hPacketFieldId': 118, 'hPacketFieldType': 'number'}"
