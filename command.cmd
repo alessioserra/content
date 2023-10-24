c885dc0acf60596a5692c4b3d6c437a5e720ea0b
Alessio Serra <48280936+alessioserra@users.noreply.github.com>
2023-10-24 10:29:13 +0200

    Create command.cmd

---
+++ command.cmd
@@ -0,0 +1,2 @@
+/spark/bin/spark-submit --packages org.apache.spark:spark-avro_2.13:3.4.1 /home/proveAle/content/prova24.py 17 240297 "ProvaAlgoritmo" "{'hPacketId': 117,
+ 'hPacketFieldId': 118, 'hPacketFieldType': 'number'}"
